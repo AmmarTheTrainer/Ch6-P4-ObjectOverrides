465aec6c8dc80e7507e417a824758da987df0dde
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-08-08 08:29:28 +0500

    first commit from park

---
+++ Ch6-P4-ObjectOverrides/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch6_P4_ObjectOverrides
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
