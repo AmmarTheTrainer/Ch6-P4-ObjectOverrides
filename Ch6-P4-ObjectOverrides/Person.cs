6d639319893b5c2547fee9794b2214ca3c3bbc26
Ammar The Trainer <Ammarthetrainer@gmail.com>
2018-08-09 02:51:28 +0500

    commit 3 from hostel KQ

---
+++ Ch6-P4-ObjectOverrides/Person.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ch6_P4_ObjectOverrides
+{
+    class Person
+    {
+        public string FirstName { get; set; } = "";
+        public string LastName { get; set; } = "";
+        public int Age { get; set; }
+
+        public Person() { }
+        public Person(string fName, string lName, int personAge)
+        {
+            FirstName = fName;
+            LastName = lName;
+            Age = personAge;
+        }
+
+
+        public override string ToString() => $"[First Name: {FirstName}; Last Name: {LastName}; Age: {Age}]";
+        
+    }
+}
